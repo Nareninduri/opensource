34a8449f7f93eb8a7b0e7513a363cb7e770f3d95
Nareninduri <naren0667@gmail.com>
2024-11-12 10:28:26 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,12 @@
+import.java.util;
+class main{
+  public static void main(String[]args){
+    Scanner sc=new Scanner(System.in);
+    int x=sc.nextInt();
+    if(x%2) {
+      System.out.println("even");
+    }
+    else {
+      System.out.println("odd");
+    }
+  }
